e8e3924da716344c731f6ee1dbb426cd5deb75bf
Eric Knigge <ericknigge@gmail.com>
2023-04-24 21:38:50 -0700

    updates for removing zip codes

--- data/process_data.py
+++ data/process_data.py
@@ -3,6 +3,14 @@ import os
 import shutil
 
 
+def read_zip_code_file(filename: str) -> list:
+    out = []
+    with open(filename, 'r') as f:
+        for line in f:
+            out.append(int(line.strip('\n')))
+    return out
+        
+
 def add_zip_data(df_data: pd.DataFrame, df_zip_tracts: pd.DataFrame) -> pd.DataFrame:
     df_data = df_data.merge(df_zip_tracts, left_on='Geographic Identifier - FIPS Code', right_on='tract')
     return df_data
@@ -23,7 +31,7 @@ def main():
 
     # get csv files in directory
     current_directory = os.getcwd()
-    output_directory_name = 'zip_code_added'
+    output_directory_name = 'processed_data'
     excluded_csv_files = ['TRACT', 'zip_tracts', 'temp', 'Pet_Licenses']
     csv_files = [i for i in os.listdir(os.getcwd()) if 'csv' in i]
     csv_files = drop_excluded(csv_files, excluded_csv_files)
@@ -44,6 +52,16 @@ def main():
         os.chdir(current_directory+'\\'+output_directory_name)
         df.to_csv(output_filename)
         os.chdir(current_directory)
+    
+    # remove non-Seattle zip code data from dataset
+    pet_data_filename = 'Seattle_Pet_Licenses.csv'
+    zip_code_filename = 'seattle_zip_codes.txt'
+    df_license = pd.read_csv(pet_data_filename)
+    zip_codes = read_zip_code_file(zip_code_filename)
+    df_license = df_license[df_license['ZIP Code'].isin(zip_codes)]
+    os.chdir(current_directory+'\\'+output_directory_name)
+    df_license.to_csv(pet_data_filename)
+
 
 
 if __name__ == '__main__':
