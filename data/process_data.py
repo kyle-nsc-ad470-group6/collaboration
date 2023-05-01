56bcf6bc45c35dac8b814d93247b2a43060947fa
Eric Knigge <ericknigge@gmail.com>
2023-04-30 22:37:48 -0700

    week 5 updates

--- data/process_data.py
+++ data/process_data.py
@@ -9,11 +9,13 @@ def read_zip_code_file(filename: str) -> list:
         for line in f:
             out.append(int(line.strip('\n')))
     return out
-        
+
 
 def add_zip_data(df_data: pd.DataFrame, df_zip_tracts: pd.DataFrame) -> pd.DataFrame:
-    df_data = df_data.merge(df_zip_tracts, left_on='Geographic Identifier - FIPS Code', right_on='tract')
+    df_data = df_data.merge(
+        df_zip_tracts, left_on='Geographic Identifier - FIPS Code', right_on='tract')
     return df_data
+
 
 def drop_excluded(items: list, exclusions: list) -> list:
     out = []
@@ -52,7 +54,7 @@ def main():
         os.chdir(current_directory+'\\'+output_directory_name)
         df.to_csv(output_filename)
         os.chdir(current_directory)
-    
+
     # remove non-Seattle zip code data from dataset
     pet_data_filename = 'Seattle_Pet_Licenses.csv'
     zip_code_filename = 'seattle_zip_codes.txt'
